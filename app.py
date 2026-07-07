26e3b5db788f67d460ea911c285a8362c5da806c
illyan-web <nur_illya_nafiza_mohamed@moe.edu.sg>
2026-07-07 21:14:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # App title
-st.title("Simple Streamlit Demo")
+st.title("Illya's First App")
 
 # Header and subheader
 st.header("Welcome to My App")
